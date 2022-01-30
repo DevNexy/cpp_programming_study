c86ca7ddb14c13d0caf5ac4f27dc6572dafde10a
Jiwon Song <92451281+devjisong@users.noreply.github.com>
2022-01-31 01:03:04 +0900

    Ex3_5

---
+++ ex3_5.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+class Point {
+	int x, y;
+public:
+	Point();
+	Point(int a, int b);
+	void show() {
+		cout << "(" << x << ", " << y << ")" << endl;
+	}
+};
+
+Point::Point() : Point(0, 0) { } //Point(int a, int b) 생성자 호출
+
+Point::Point(int a, int b)
+	: x(a), y(b) { }
+
+int main() {
+	Point origin;
+	Point target(10, 20);
+	origin.show();
+	target.show();
+}
