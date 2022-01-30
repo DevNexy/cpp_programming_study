16fcd1dd654c8ce7d1ae0286f150c0456914d07f
Jiwon Song <92451281+devjisong@users.noreply.github.com>
2022-01-31 00:00:10 +0900

    Ex2_4

---
+++ ex2_4.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "이름을 입력하세요>>";
+
+	char name[11]; //한글은 5개 글자, 영문은 10개까지 저장할 수 있다.
+	cin >> name; //키보드로부터 문자열을 읽는다.
+
+	cout << "이름은 " << name << "입니다\n"; //이름을 출력한다.
+}
