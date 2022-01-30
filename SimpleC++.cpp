3a5e1c080b5679b26ece74810f4082471783d636
Jiwon Song <92451281+devjisong@users.noreply.github.com>
2022-01-30 23:58:03 +0900

    Ex2_1

---
+++ SimpleC++.cpp
@@ -0,0 +1,13 @@
+/*
+	소스 : SimpleC++.cpp
+	cout과 << 연산자를 이용하여 화면에 출력한다.
+*/
+
+#include <iostream> //cout과 << 연산자 포함
+
+// C++ 프로그램은 main() 함수에서부터 실행을 시작한다.
+int main() {
+	std::cout << "Hello\n";
+	std::cout << "첫 번째 맛보기입니다.";
+	return 0; //main() 함수가 종료하면 프로그램이 종료됨
+}
